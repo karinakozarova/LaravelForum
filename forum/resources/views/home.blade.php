9b53e630125cca364c6461c9ddd228f1a1288a90
Karina Kozarova <karinakozarova1303@gmail.com>
2020-05-24 16:58:21 +0200

    added login, registration and dashboard page

--- forum/resources/views/home.blade.php
+++ forum/resources/views/home.blade.php
@@ -1,23 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <h3 class="title center"> Dashboard </h3> <br>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
 
-                    You are logged in!
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">New forum posts</div>
+
+                    <div class="card-body">
+
+                    </div>
+                </div>
+                <br> <br>
+                <div class="card">
+                    <div class="card-header">Recently commented</div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+
+                <br> <br>
+
+                <div class="card">
+                    <div class="card-header">Newly joined</div>
+                    <div class="card-body">
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
