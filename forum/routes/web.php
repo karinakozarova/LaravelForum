0a4e6df251a33a8cf3a24e260f9d277f4ba9afee
Karina Kozarova <karinakozarova1303@gmail.com>
2020-05-24 16:59:55 +0200

    change route

--- forum/routes/web.php
+++ forum/routes/web.php
@@ -17,7 +17,7 @@ Route::get('', function () {
     return view('welcome');
 });
 
-Route::get('/hello-world', function () {
+Route::get('/index', function () {
     return view('dashboard');
 });
 Auth::routes();
